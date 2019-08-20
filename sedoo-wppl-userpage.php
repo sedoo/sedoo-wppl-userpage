7510c7efbc641f809e7a511693b30460cf74eb8a
Pierre VERT <vert@obs-mip.fr>
2019-08-20 17:51:24 +0200

    initial commit

---
+++ sedoo-wppl-userpage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Sedoo Wppl Userpage
+ * Plugin URI:      https://github.com/sedoo/sedoo-wppl-userpage
+ * Description:     Pages profils utilisateurs
+ * Author:          Pierre VERT - SEDOO DATA CENTER
+ * Author URI:      https://www.sedoo.fr 
+ * Text Domain:     sedoo-wppl-userpage
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ * GitHub Plugin URI: sedoo/sedoo-wppl-userpage
+ * GitHub Branch:     master
+ * @package         Sedoo_Wppl_Userpage
+ */
+
+// Your code starts here.
